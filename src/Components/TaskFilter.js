18ac11971d176b301e8e3bb8c1ddc264b0b11a54
PriyaDharshini-Sysvine <priya.dharshini@sysvine.com>
2024-11-20 17:07:23 +0530

    TaskContext, TaskManager(Custom hook), TaskFilter

---
+++ src/Components/TaskFilter.js
@@ -0,0 +1,23 @@
+import { useTaskManager } from "./TaskContext";
+
+export function TaskFilter() {
+    var { filterTasks } = useTaskManager();
+
+    function onFilterChange(event) {
+        if (event == undefined)
+            return;
+        filterTasks(event.target.value);
+    }
+
+    return (
+        <div>
+            <label>Filter by Status: </label>
+            <select onChange={(e) => onFilterChange(e)} >
+                <option value="All">All</option>
+                <option value="Completed">Completed</option>
+                <option value="Pending">Pending</option>
+            </select>
+        </div>
+    );
+
+}
